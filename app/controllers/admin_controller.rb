984df5da64600fe052768b87a9bbd3e4cebea036
Arvind kushwah <arvindkushwah9@gmail.com>
2019-09-15 21:32:58 +0530

    Fix admin home page error

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,10 +4,10 @@ class AdminController < ApplicationController
  
   def index
    if params[:search].present?
-      @products = Product.where("lower(title) LIKE :prefix OR lower(description) LIKE :prefix",prefix: "%#{params[:search].downcase}%")
+      @products = Product.where("lower(title) LIKE :prefix OR lower(description) LIKE :prefix",prefix: "%#{params[:search].downcase}%").paginate(page: params[:page], per_page: 4)
     elsif params[:category_id].present?
       @cat = Category.find(params[:category_id])
-      @products = @cat.products
+      @products = @cat.products.paginate(page: params[:page], per_page: 4)
     else
       @products = Product.paginate(page: params[:page], per_page: 4)
     end
