1ca1c955a5254a75ccbac87195e0fedb57733848
Arvind kushwah <arvindkushwah9@gmail.com>
2021-05-22 13:40:35 +0530

    cange

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   layout 'admin'
   before_action :authentication_admin!
  
-  def index
+  def index    
    if params[:search].present?
       @products = Product.where("lower(title) LIKE :prefix OR lower(description) LIKE :prefix",prefix: "%#{params[:search].downcase}%").paginate(page: params[:page], per_page: 4)
     elsif params[:category_id].present?
